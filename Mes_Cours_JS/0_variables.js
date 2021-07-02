96737033d016233aeeb54d72ea6765091bd02297
nal95 <alex.nguepi@yahoo.fr>
2021-07-02 12:57:32 +0200

    les variables

--- Mes_Cours_JS/0_variables.js
+++ Mes_Cours_JS/0_variables.js
@@ -1,1 +1,12 @@
-alert('Bonjour le monde');
+//alert('Bonjour le monde');
+//Déclaration & affectation de variables
+
+//une variable
+let age = 25;
+console.log(age);
+age = 30;
+console.log(age);
+
+//une constante
+const constante ='Je suis dans une console'; 
+console.log(constante);
