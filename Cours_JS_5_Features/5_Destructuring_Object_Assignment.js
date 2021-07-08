00fe1dc79038f4431edea7b993b470cb09a15419
nal95 <alex.nguepi@yahoo.fr>
2021-07-08 12:06:22 +0200

    destructuring Object

---
+++ Cours_JS_5_Features/5_Destructuring_Object_Assignment.js
@@ -0,0 +1,29 @@
+
+//La puissance de la déstructuration d'affectation est plus utilisée pour les objects
+const personne1 = {
+    nom : 'Musole',
+    postNom : 'Gloire',
+    age : 32,
+    addresse : {
+        pays : 'xxx',
+        ville : 'yyy'
+    }
+}
+
+//Déstructuraction d'un object
+const {nom, age, addresse : {ville, pays}} = personne1;
+console.log(nom, age, pays, ville);
+
+const personne2 = {...personne1, nom : 'Blaise'}
+console.log(nom, personne2);
+
+//Fonction avec destructuration en paramètre (argument)
+const affichagePersonnes = ({nom, postNom, age, addresse : {pays, ville}}) => {
+    console.log(
+        `${nom} ${postNom} a ${age} ans et son addresse est
+        ${pays} - ${ville}`
+        );
+};
+
+affichagePersonnes(personne1);
+affichagePersonnes(personne2);
