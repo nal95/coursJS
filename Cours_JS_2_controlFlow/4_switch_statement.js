2afe1738d850dada9fab66bc3318ad09316d8513
nal95 <alex.nguepi@yahoo.fr>
2021-07-08 08:24:20 +0200

    while  , if & switch statement

---
+++ Cours_JS_2_controlFlow/4_switch_statement.js
@@ -0,0 +1,17 @@
+const mention = 'P';
+
+switch (mention) {
+    case 'A':
+        console.log('Vous avez reussi avec mention A');
+        break;
+    case 'B':
+        console.log('Vous avez reussi avec mention B');
+        break;
+    case 'C':
+        console.log('Vous avez reussi avec mention C');
+        break;
+
+    default:
+        console.log("la mention n'est pas attribuer");
+        break;
+}
