2e691af0c04154ec2ca5c6c8652f70cf155e8665
nal95 <alex.nguepi@yahoo.fr>
2021-07-08 08:07:36 +0200

    for & while schleife

---
+++ Cours_JS_2_controlFlow/2_boucleWhile.js
@@ -0,0 +1,20 @@
+let i = 0;
+while(i<5){
+    console.log('dans la boucle i est :',i);
+    i++;
+}
+
+//Parcourir une liste des noms avec la boucle [While]
+let noms = ['Musole', 'Blaise', 'Jonathan', 'Gloria'];
+let j = 0;
+while(j < noms.length){
+    console.log(j, noms[j]);
+    j++;
+}
+
+//Methode pour faire l'excution d'un bloque des codes avent la verification de la condition
+let x = 6;
+do{
+    console.log('la valeur de x est: ', x);
+    x++;
+}while(x<5);
